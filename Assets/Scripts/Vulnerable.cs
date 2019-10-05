3e60e5910c023b55bba68e04b75751238edd637c
spencer_dowie <spencer.dowie@uoit.net>
2019-10-05 14:59:10 -0400

    [+] Vulnerable script

---
+++ Assets/Scripts/Vulnerable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+   public  enum AttackType
+    {
+        Jump = 0,
+        Punch
+    }
+
+public class Vulnerable : MonoBehaviour
+{
+    public AttackType vulnerableTo;
+
+    public bool RecieveAttack(AttackType incomingType)
+    {
+        return incomingType == vulnerableTo;
+    }
+}
