7fc273e6845c60e9b4e23ba22d64476968c97d04
Tyler Stein <tyler.stein1@yahoo.ca>
2019-10-06 21:17:49 -0400

    WIP: AOEBlastFloater

---
+++ Assets/AOEAttackController.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public enum AOEState
+{
+    IDLE = 0,
+    GROW = 1,
+    STAY = 2,
+    SHRINK = 3
+}
+
+public class AOEAttackController : MonoBehaviour
+{
+    [SerializeField] public AOEState State { get { return _state;  } }
+
+    [SerializeField] public Transform rootTransform;
+    [SerializeField] public SpriteRenderer spriteRenderer;
+    [SerializeField] public CircleCollider2D circleCollider;
+
+    [SerializeField] public float minSize = 0.1f;
+    [SerializeField] public float maxSize = 1.0f;
+
+    [SerializeField] public float errorMargin = 0.025f;
+
+    [SerializeField] public float growthDampening = 50f;
+    [SerializeField] public float shrinkDampening = 25f;
+    [SerializeField] public float stayDuration = 3.0f;
+
+    [SerializeField] private AOEState _state = AOEState.IDLE;
+    [SerializeField] private bool _didHitPlayer = false;
+    [SerializeField] private float _scaleDampenVelocity = 0f;
+    [SerializeField] private float _currentScale = 0f;
+    [SerializeField] private float _stayTicker = 0f;
+
+    private void Start() {
+        Deactivate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_state == AOEState.GROW) {
+            _currentScale = Mathf.SmoothDamp(_currentScale, maxSize, ref _scaleDampenVelocity, growthDampening * Time.deltaTime);
+            setScales();
+            if (_currentScale >= (maxSize - errorMargin)) {
+                _state = AOEState.STAY;
+                _stayTicker = 0f;
+                _scaleDampenVelocity = 0f;
+            }
+        } else if (_state == AOEState.STAY) {
+            if (_stayTicker < stayDuration) {
+                _stayTicker += Time.deltaTime;
+            }
+
+            if (_stayTicker >= stayDuration) {
+                _state = AOEState.SHRINK;
+            }
+        } else if (_state == AOEState.SHRINK) {
+            _currentScale = Mathf.SmoothDamp(_currentScale, minSize, ref _scaleDampenVelocity, shrinkDampening * Time.deltaTime);
+            setScales();
+            if (_currentScale <= (minSize + errorMargin)) {
+                Deactivate();
+            }
+        }
+    }
+
+    public void Activate() {
+        _state = AOEState.GROW;
+        _currentScale = minSize;
+        _didHitPlayer = false;
+        _scaleDampenVelocity = 0f;
+        setScales();
+        spriteRenderer.enabled = true;
+        circleCollider.enabled = true;
+    }
+
+    public void Deactivate() {
+        _state = AOEState.IDLE;
+        _currentScale = minSize;
+        _scaleDampenVelocity = 0f;
+        spriteRenderer.enabled = false;
+        circleCollider.enabled = false;
+        _didHitPlayer = false;
+        setScales();
+    }
+
+    private void setScales() {
+        rootTransform.localScale = new Vector3(_currentScale, _currentScale, rootTransform.localScale.z);
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            if (!_didHitPlayer) {
+                Debug.Log("oof bud");
+                _didHitPlayer = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FloaterMovementControllerr.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloaterMovementControllerr : MonoBehaviour
+{
+    public bool IsAtHoverHeight { get { return Mathf.Abs(movementSettings.hoverHeight - transform.position.y) < movementSettings.hoverErrorMargin; } }
+
+    // Last direction of movement (by input)
+    public float LastDirection { get { return _lastDirection; } }
+
+    public bool IsBlocked { get { return _isBlocked; } }
+
+    // Is the player colliding down
+    public bool IsGrounded { get { return _isGrounded; } }
+
+    // Is the controller colliding upw
+    public bool IsTouchingCeiling { get { return _isTouchingCeiling; } }
+
+    // Int defines direction of wall, 0 means none
+    public int TouchingWallDirection { get { return _touchingWallDirection; } }
+
+    // Rigidbody's current velocity
+    public Vector2 Velocity { get { return _rigidbody.velocity; } }
+
+    [SerializeField] public FloaterMovementSettings movementSettings;
+
+    [SerializeField] private Transform _transform;
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private RaycastHit2D[] _contacts = new RaycastHit2D[3];
+    [SerializeField] private Vector2 _currentVelocity = Vector2.zero;
+    [SerializeField] private bool _shouldJump = false;
+    [SerializeField] private float _lastDirection = 1f;
+    [SerializeField] private bool _isGrounded = false;
+    [SerializeField] private int _touchingWallDirection = 0;
+    [SerializeField] private bool _isTouchingCeiling = false;
+    [SerializeField] private bool _didMoveLastFrame = false;
+    [SerializeField] private bool _isBlocked = false;
+
+
+    public void Start() {
+        if (!_transform) _transform = GetComponent<Transform>();
+        if (!_rigidbody) _rigidbody = GetComponent<Rigidbody2D>();
+        if (!_collider) _collider = GetComponent<BoxCollider2D>();
+
+        _rigidbody.isKinematic = false;
+        _rigidbody.simulated = true;
+        _rigidbody.freezeRotation = true;
+    }
+
+    public void FixedUpdate() {
+        updateTouchingWalls();
+
+        if (!_didMoveLastFrame) {
+            dampenMovement();
+        }
+        _didMoveLastFrame = false;
+
+        hover();
+
+        updateTouchingCeiling();
+        updateBlocked();
+    }
+
+    public void Move(float direction) {
+        _lastDirection = Mathf.Sign(_rigidbody.velocity.x);
+
+        // prevent wall sticking
+        if (Mathf.Sign(direction) == _touchingWallDirection) {
+            direction = 0;
+        }
+
+        // prevent affecting velocity with move when input is 0
+        _didMoveLastFrame = direction > 0f || direction < 0f;
+        if (!_didMoveLastFrame) return;
+
+        float desiredDirection = Mathf.Sign(direction);
+        Vector2 targetVelocity = new Vector2(direction * movementSettings.airMoveVelocity, _rigidbody.velocity.y);
+        _rigidbody.velocity = Vector2.SmoothDamp(_rigidbody.velocity, targetVelocity, ref _currentVelocity, movementSettings.airMoveSmoothing);
+    }
+
+    public void Update() {
+        //
+    }
+
+    public void ClearVelocity() {
+        _rigidbody.velocity = Vector2.SmoothDamp(_rigidbody.velocity, Vector2.zero, ref _currentVelocity, 0.0001f);
+    }
+
+    public void ClearVelocityX() {
+        _rigidbody.velocity = Vector2.SmoothDamp(_rigidbody.velocity, new Vector2(0, _rigidbody.velocity.y), ref _currentVelocity, 0.0001f);
+    }
+
+    public void AddForce(Vector2 force) {
+        _rigidbody.AddForce(force, ForceMode2D.Impulse);
+    }
+
+    private void hover() {
+        float heightDifference = movementSettings.hoverHeight - transform.position.y;
+        Vector2 targetVelocity = Vector2.zero;
+        if (heightDifference > movementSettings.hoverErrorMargin) {
+            // go up
+            targetVelocity = new Vector2(_rigidbody.velocity.x, movementSettings.hoverHeightSpeed);
+        } else if (heightDifference < -movementSettings.hoverErrorMargin) {
+            // go down
+            targetVelocity = new Vector2(_rigidbody.velocity.x, -movementSettings.hoverHeightSpeed);
+        } else {
+            // hover here
+            targetVelocity = new Vector2(_rigidbody.velocity.x, 0);
+        }
+        _rigidbody.velocity = Vector2.SmoothDamp(_rigidbody.velocity, targetVelocity, ref _currentVelocity, movementSettings.hoverHeightSmoothing);
+    }
+
+    private void dampenMovement() {
+        Vector2 targetVelocity = new Vector2(0, _rigidbody.velocity.y);
+        if (_currentVelocity.x < 0.01f) _currentVelocity.Set(0, _currentVelocity.y);
+        _rigidbody.velocity = Vector2.SmoothDamp(_rigidbody.velocity, targetVelocity, ref _currentVelocity, movementSettings.airStopSmoothing);
+    }
+
+    private void updateTouchingWalls() {
+        if (_lastDirection < 0) {
+            // looking left, test left first
+            if (isTouching(Vector2.left, movementSettings.minWallDistance, movementSettings.wallLayer)) {
+                _touchingWallDirection = -1;
+            } else if (isTouching(Vector2.right, movementSettings.minWallDistance, movementSettings.wallLayer)) {
+                _touchingWallDirection = 1;
+            } else {
+                _touchingWallDirection = 0;
+            }
+        } else {
+            // looking right, test right first
+            if (isTouching(Vector2.right, movementSettings.minWallDistance, movementSettings.wallLayer)) {
+                _touchingWallDirection = 1;
+            } else if (isTouching(Vector2.left, movementSettings.minWallDistance, movementSettings.wallLayer)) {
+                _touchingWallDirection = -1;
+            } else {
+                _touchingWallDirection = 0;
+            }
+        }
+    }
+
+    private void updateTouchingCeiling() {
+        _isTouchingCeiling = isTouching(Vector2.up, movementSettings.minCeilingDistance, movementSettings.ceilingLayer);
+    }
+
+    private void updateBlocked() {
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.ClearLayerMask();
+
+        int contactCount = _collider.Cast(Vector2.down, filter, _contacts, movementSettings.minGroundDistance);
+        for (int i = 0; i < contactCount; i++) {
+            if (_contacts[i].collider != null && _contacts[i].transform != transform) {
+                _isBlocked = true;
+                if (_contacts[i].collider.tag == "Ground") {
+                    _isGrounded = true;
+                } else {
+                    _isGrounded = false;
+                }
+                return;
+            }
+        }
+        _isGrounded = false;
+        _isBlocked = false;
+    }
+
+    private bool isTouching(Vector2 direction, float distance, int mask = 1 << 0) {
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.layerMask = mask;
+
+
+        int contactCount = _collider.Cast(direction, filter, _contacts, distance);
+        for (int i = 0; i < contactCount; i++) {
+            if (_contacts[i].collider != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
