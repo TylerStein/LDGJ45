75071bda394cf962e3fd1906b9803edbb5fd0da1
A-Aultman <56202178+A-Aultman@users.noreply.github.com>
2019-10-07 13:34:52 -0400

    Audio
    Added Audio clips, and audio source/and unfinished script to enemies and player

---
+++ Assets/Enemy_Sound_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Sound_Controller : MonoBehaviour
+{
+
+    public AudioClip Attack, Die;
+    AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
